300c15e3a0ef6e90f4b5b406acc6987a0f2d520c
Magdalena Miszewska <mag.miszewska@gmail.com>
2018-04-10 19:08:10 +0200

    Update js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
+var buttons = document.getElementsByClassName('button'),
+    buttonQuantity = buttons.length,
+    textContent = "";
+for (var i = 0; i < buttonQuantity; i++) {
+    textContent = buttons[i];
+    alert(textContent.innerHTML);
+}
+
+
+
+/*
 var buttons = document.getElementsByClassName('button');
 
 for (var i = 0; i < buttons.length; i++) {
     window.alert(buttons[i].innerText);
 }
+*/
